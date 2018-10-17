84b549c149592cdf4048dd23064647b01c91e197
Kunal Katdare <15366226+Kunalkatdare@users.noreply.github.com>
2018-10-17 15:08:54 -0400

    Just some UI changes and Code review

---
+++ javascript/cred.js
@@ -0,0 +1,58 @@
+var apigClient = apigClientFactory.newClient();
+
+var apigClient = apigClientFactory.newClient({
+  accessKey: "AKIAIMBCT7C3M5QCCGGA",
+  secretKey: "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"
+});
+
+var API_KEY = "vpEIYVEiJQ4W4EjmlNP8d6jP0HwaAZEWaVuJD30Q";
+
+var apigClient = apigClientFactory.newClient({
+  apiKey: API_KEY
+});
+
+var params = {
+  // This is where any modeled request parameters should be added.
+  // The key is the parameter name, as it is defined in the API in API Gateway.
+  //   param0: "",
+  //   param1: ""
+};
+
+var additionalParams = {
+  // If there are any unmodeled query parameters or headers that must be
+  //   sent with the request, add them here.
+  //   headers: {
+  //     "Access-Control-Allow-Origin": "*"
+  //   }
+};
+
+function postRequest(body) {
+  apigClient
+    .chatbotPost(params, body, additionalParams)
+    .then(function(result) {
+      text = parseServerResponse(result);
+      insertChat("you", text);
+    })
+    .catch(function(result) {
+      console.log(result);
+    });
+}
+
+function parseServerResponse(response) {
+  msg_list = response.data.message;
+
+  if (msg_list === undefined) {
+    text = "Sorry I dont have an answer right now!";
+    return text;
+  }
+
+  max = msg_list.length - 1;
+  min = 0;
+  rand = generateRandomInteger(min, max);
+  text = msg_list[rand];
+  return text;
+}
+
+function generateRandomInteger(min, max) {
+  return Math.floor(min + Math.random() * (max + 1 - min));
+}
---
+++ javascript/dynamic.js
@@ -0,0 +1,91 @@
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  var strTime = hours + ":" + minutes + " " + ampm;
+  return strTime;
+}
+
+//-- No use time. It is a javaScript effect.
+function insertChat(who, text, time) {
+  if (time === undefined) {
+    time = 0;
+  }
+  var control = "";
+  var date = formatAMPM(new Date());
+
+  if (who == "me") {
+    control =
+      '<li style="width:100%">' +
+      '<div class="msj macro">' +
+      '<div class="avatar"><img class="img-circle" style="width:100%;" src="' +
+      me.avatar +
+      '" /></div>' +
+      '<div class="text text-l">' +
+      "<p>" +
+      text +
+      "</p>" +
+      "<p><small>" +
+      date +
+      "</small></p>" +
+      "</div>" +
+      "</div>" +
+      "</li>";
+    body = buildChatMessage(text, date);
+  } else {
+    control =
+      '<li style="width:100%;">' +
+      '<div class="msj-rta macro">' +
+      '<div class="text text-r">' +
+      "<p>" +
+      text +
+      "</p>" +
+      "<p><small>" +
+      date +
+      "</small></p>" +
+      "</div>" +
+      '<div class="avatar" style="padding:0px 0px 0px 10px !important"><img class="img-circle" style="width:100%;" src="' +
+      you.avatar +
+      '" /></div>' +
+      "</li>";
+    body = "";
+  }
+
+  setTimeout(function() {
+    $("ul")
+      .append(control)
+      .scrollTop($("ul").prop("scrollHeight"));
+  }, 100);
+  return body;
+}
+
+//-- To send to the server
+
+function buildChatMessage(text, date) {
+  //Used fot every messageÏ
+  var unstructured = {
+    id: "string",
+    text: text,
+    timestamp: date
+  };
+
+  var message = {
+    type: "string",
+    unstructured: unstructured
+  };
+
+  var messages = [];
+
+  messages.push(message);
+
+  var requestBody = {
+    messages: messages
+  };
+
+  console.log(JSON.stringify(requestBody));
+
+  return requestBody;
+}
